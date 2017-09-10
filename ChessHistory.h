82b66f7666098c089c602f64a083c21c6649e4e3
uri <uri@t2ki.co.il>
2017-09-10 10:51:27 +0300

    ChessHistory docs

--- ChessHistory.h
+++ ChessHistory.h
@@ -16,12 +16,14 @@ typedef struct chess_move_history{
     char soldierDied;
 }HistoryNode;
 
+// new type used to hold chess moves
 typedef struct chess_array_list{
     HistoryNode* moves;
     int actualSize;
     int maxSize;
 }ChessHistory;
 
+// enum used for chess game messages
 typedef enum{
     SUCCESS,
     ILLEGAL_MOVE,
@@ -33,16 +35,44 @@ typedef enum{
 
 }CHESS_MESSAGE;
 
+/**
+ * creates a struct to hold a given amount (the input) of chess game moves
+ *
+ * @return a pointer to the new struct
+ */
 ChessHistory* createChessHistory(int maxSize);
 
+/**
+ * copies a given ChessHistory
+ *
+ * @return a pointer to the new struct
+ */
 ChessHistory * copyChessHistory(ChessHistory* source);
 
+/**
+ * deletes a ChessHistory struct, frees all resources
+ *
+ */
 void destroyChessHistory(ChessHistory* source);
 
+/**
+ * add a given move to the given ChessHistory
+ *
+ * @return message that indicates success of failure
+ */
 CHESS_MESSAGE addMovetoHistory(ChessHistory* source, HistoryNode move);
 
+/**
+ * remove the most recent added move from the given ChessHistory (LIFO order)
+ *
+ * @return message that indicates success of failure
+ */
 HistoryNode* removeRecentMove(ChessHistory* source);
 
+/**
+ *
+ * @return true if the given ChessHistory is empty, false otherwise
+ */
 bool isHistoryEmpty(ChessHistory* source);
 
 
